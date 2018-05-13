885468356475a5b22cd31ab02813e79f7f3a6595
James Delemar <delemar@HDDELE1001.local>
2018-05-13 12:42:38 -0400

    Added crossorigin attrib to script

--- docs/app.bundle.js
+++ docs/app.bundle.js
@@ -59,7 +59,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "1e310bc7662f1a79d506"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "1d1c3e2e78d73d830d8c"; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentParents = []; // eslint-disable-line no-unused-vars
@@ -21452,7 +21452,7 @@ module.exports = function (data, opts) {
 /***/ "./node_modules/file-loader/index.js!./src/index.html":
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__.p + "6cab51f9a1c30450b8b2ac8d89bb32eb.html";
+module.exports = __webpack_require__.p + "44c018a2fcffb6a5bd33d92a5e6971a7.html";
 
 /***/ }),
 
